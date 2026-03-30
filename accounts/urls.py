3b6d72b31cc381e53b41958742c11e10800a4561
Suraj Bodake_c <suraj.bodake_c@sandvik.com>
2026-03-30 11:00:26 +0530

    fix homepage route

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login_view),
     path('signup/', views.signup),
     path('login/', views.login_view),
     path('dashboard/', views.dashboard),
