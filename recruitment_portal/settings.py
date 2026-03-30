b713a5fb0f25cc3bdbcca0dda3342b250eb8cb58
Suraj Bodake_c <suraj.bodake_c@sandvik.com>
2026-03-30 14:18:02 +0530

    fix allowed hosts

--- recruitment_portal/settings.py
+++ recruitment_portal/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['*']  # allow all for now (production fix later)
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # Applications
 INSTALLED_APPS = [
